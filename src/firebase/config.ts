dae253b2ddf1e4d614c10e1ae844173ae088848d
noob-anand <anandsharma8015@gmail.com>
2025-12-28 20:51:54 +0530

    Initial commit

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-3964701412-70200",
-  "appId": "1:939023173566:web:32ca4154c413dca0529bef",
-  "apiKey": "AIzaSyDoPUNrWFsZ5SrngI1kXK-x099lx3a-4I8",
-  "authDomain": "studio-3964701412-70200.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "939023173566"
-};
